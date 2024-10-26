f7c6a6f2e3780e4436f3ae24bfa61706421b6a91
howard leslie <howard@adaremit.co.id>
2024-10-26 14:00:16 +0700

    dajhfbvljshdfv

---
+++ App/controllers/ForgotPassController.js
@@ -0,0 +1,27 @@
+const BaseController = require("./BaseController");
+
+class ForgotPassController extends BaseController {
+  constructor() {
+    super();
+    this.view = "forgotpass";
+    this.layout = "plain";
+    this.title = "Forgot Password";
+  }
+  index(req, res) {
+    try {
+      const options = {
+        layout: `components/${this.layout}`,
+        title: this.title,
+      };
+
+      this.renderView(res, options);
+    } catch (error) {
+      this.handleError(res, "Failed to render home page", 500);
+    }
+  }
+  sendOTP() {
+    //
+  }
+}
+
+module.exports = ForgotPassController;
