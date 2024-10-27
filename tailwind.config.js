b6dd3a584abe8e812c74829d26dc21ae6d102e8c
EmpthyLL <javileh40@gmail.com>
2024-10-27 20:54:47 +0700

    login register

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "node_modules/preline/dist/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        c1: "1000px",
+        xs: "480px",
+      },
+    },
   },
   plugins: [require("preline/plugin")],
 };
