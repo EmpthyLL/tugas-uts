e9d450d4d1518a453cba1d5a1dadd713d047b8c2
Araliana <sharrenbj1@gmail.com>
2024-11-06 13:17:52 +0700

    detail done i guess

--- App/controllers/ProductController.js
+++ App/controllers/ProductController.js
@@ -7,6 +7,7 @@ class ProductController extends Controller {
     this.layout = "layout";
   }
 
+  // Render standalone product page (productDetail.ejs)
   async index(req, res) {
     try {
       const id = req.params.id;
@@ -32,7 +33,7 @@ class ProductController extends Controller {
         product,
         relatedProducts: limitedRelatedProducts,
         keyword: "",
-        categoryName: product.category.toLowerCase().replace(/\s+/g, '-') // Pass category name for URL
+        categoryName: product.category.toLowerCase().replace(/\s+/g, '-') 
       };
 
       this.renderView(res, "productDetail", options);
@@ -42,6 +43,7 @@ class ProductController extends Controller {
     }
   }
 
+  // Render product page within a category context (detailCategory.ejs)
   async productInCategory(req, res) {
     try {
       const { categoryName, id } = req.params;
@@ -67,7 +69,7 @@ class ProductController extends Controller {
         product,
         relatedProducts: limitedRelatedProducts,
         keyword: "",
-        categoryName: categoryName.toLowerCase().replace(/\s+/g, '-')
+        categoryName: categoryName.toLowerCase().replace(/\s+/g, '-') 
       };
 
       this.renderView(res, "productDetail", options);
