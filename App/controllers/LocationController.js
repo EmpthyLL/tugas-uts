e4334157f1337ae2e41e59f8d1fe1ce726d152e9
Araliana <sharrenbj1@gmail.com>
2024-11-09 03:10:01 +0700

    give up due to map

---
+++ App/controllers/LocationController.js
@@ -0,0 +1,44 @@
+const LocationController = require("./Controller");
+
+class LocationController extends Controller {
+  constructor() {
+    super();
+    this.view = "location"; 
+    this.layout = "layout";
+    this.title = "Location";
+  }
+
+  async index(req, res) {
+    try {
+      this.user = getAuthUser(req, res, false);  // Assuming this function gets the authenticated user
+      const userLocation = {
+        lat: 3.5952,  // Example coordinates for the user's location
+        lng: 98.6780
+      };
+      
+      // List of store locations
+      const locations = [
+        { name: 'Alfamart', lat: 3.5784, lng: 98.6789 },
+        { name: 'Indomaret', lat: 3.5953, lng: 98.6743 },
+        { name: 'Alfamidi', lat: 3.5952, lng: 98.6780 }
+      ];
+
+      const options = {
+        layout: `components/${this.layout}`,
+        title: this.title,
+        req,
+        menus: this.menus,
+        keyword: "",
+        userLocation, 
+        locations,
+      };
+
+      this.renderView(res, this.view, options);  // This will render the `location.ejs` template with the above data
+    } catch (error) {
+      console.error("Error rendering map page:", error);
+      this.handleError(res, "Failed to render map page", 500);
+    }
+  }
+}
+
+module.exports = LocationController;
