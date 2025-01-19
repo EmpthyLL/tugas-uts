2d4f44bedcc1157d243dc2ec8d976b7dc20130dd
EmpthyLL <javileh40@gmail.com>
2025-01-20 00:03:13 +0700

    dfvdfv

--- Public/js/cartHandle.js
+++ Public/js/cartHandle.js
@@ -229,6 +229,8 @@ function format(number) {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(number);
 }
 
